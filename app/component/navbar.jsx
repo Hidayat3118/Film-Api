a0daea542e7f50b0ce08df9003a7cf075a3e3c0a
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-06-16 17:02:31 +0800

    fix tampilan fix banget

--- app/component/navbar.jsx
+++ app/component/navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
                   placeholder="Search movies..."
                   value={query}
                   onChange={(e) => setQuery(e.target.value)}
-                  className="pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:ring-2 focus:ring-red-400 focus:outline-none transition w-64"
+                  className="pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:ring-2 focus:ring-red-400 focus:outline-none transition w-64 text-gray-500"
                 />
               </form>
               <FiSearch
